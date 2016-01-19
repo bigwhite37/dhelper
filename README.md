080f8917b9eaa4add308153a0a56bdbf720b3f81
szy <zhenyi.shu@upai.com>
2016-01-19 17:17:46 +0800

    fix some error

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# dhelper
-Using to start debugger for elixir by debugger module in erlang
+# Elixir Debug
+Using to start debugger for elixir by debugger module in erlang.
+
+Eesure your elixir code in a mix project.
 
 ## Usage
 ```erlang
-dhelper:start('beam_dir', 'source_dir').
+dhelper:start("beam_dir", "source_dir", "your_project_name").
+debugger:start().
+```
+### or
+```erlang
+%% make sure you are in project directory.
+dhelper:start("your_project_name").
 ```
--- src/dhelper.erl
+++ src/dhelper.erl
@@ -1,25 +1,26 @@
 -module(dhelper).
--export([start/0, start_helper/2]).
+-export([start/0, start/2]).
 
 -define(START, 8). %% START = string:len("Elixir.") + 1
 -define(END, 5).   %% END = string:len(".beam)
 -define(SRCSUFFIXLEN, 11). %% len of "defmodule"
 
-start() ->
-  start_helper("/home/zhenyi/code/upai/ele/develop/Elena.test/_build/dev/lib/elena/ebin", "/home/zhenyi/code/upai/ele/develop/Elena.test/lib"),
-  debugger:start().
+start(Project_Name) ->
+  start(filename:join(filename:absname(""), "_build/dev/lib/" ++ string:to_lower(Project_Name) ++ "/ebin"),
+        filename:join(filename:absname(""), "lib"), Project_Name)
+  debugger:start();
 
-start_helper(BeamDir, SrcDir) ->
+start(BeamDir, SrcDir, Project_Name) ->
   %% BeamFile is a filename without path info.
   BeamFMPair = get_file_names(BeamDir),
   %% SrcFile is an abs path
-  SrcFMPair = pair_src_module(SrcDir),
+  SrcFMPair = pair_src_module(SrcDir, Project_Name),
   handle_start(pair_beam_src(BeamFMPair, SrcFMPair, []), BeamDir).
 
 handle_start([], _) ->
   ok;
 handle_start([{Module, Beam, SrcPath} | MBSList], BeamDir) ->
-  BeamPath = lists:flatten([BeamDir, "/", Beam]),
+  BeamPath = filename:join(BeamDir, Beam),
   {ok, BeamBin} = file:read_file(BeamPath),
   M = list_to_atom(lists:flatten(["Elixir.", atom_to_list(Module)])),
   int:i({M, SrcPath, BeamPath, BeamBin}),
@@ -32,29 +33,29 @@ pair_beam_src(BeamFMPair, [{Module, SrcFile} | SrcRest], Acc) ->
   {Module, BeamFile} = lists:keyfind(Module, 1, BeamFMPair),
   pair_beam_src(BeamFMPair, SrcRest, [{Module, BeamFile, SrcFile} | Acc]).
 
-pair_src_module(SrcDir) ->
+pair_src_module(SrcDir, Project_Name) ->
   Srcs = os:cmd(lists:flatten(["find ", SrcDir, " | grep .ex"])),
   %% filenae in FileNames is a abs path
   FileNames = string:tokens(Srcs, "\n"),
   %% returns like this [[{module_atom, file_string}, {module_atom, file_string}], [...] ...]
-  Results = lists:map(fun(File) -> build_sm_tuple(File) end, FileNames),
+  Results = lists:map(fun(File) -> build_sm_tuple(File, Project_Name) end, FileNames),
   lists:flatten(Results).
 
-build_sm_tuple(File) ->
+build_sm_tuple(File, Project_Name) ->
   {ok, Content} = file:open(File, read),
   {ok, MP} = re:compile("defmodule", []),
-  Modules = find_module(file:read_line(Content), Content, MP, []),
+  Modules = find_module(file:read_line(Content), Content, MP, [], Project_Name),
   file:close(Content),
   lists:map(fun(Item) -> {list_to_atom(Item), File} end, Modules).
 
-find_module(eof, _, _, Modules) ->
+find_module(eof, _, _, Modules, _) ->
   Modules;
-find_module({ok, Data}, Content, MP, Modules)->
+find_module({ok, Data}, Content, MP, Modules, Project_Name)->
   case re:run(Data, MP, []) of
     nomatch ->
       find_module(file:read_line(Content), Content, MP, Modules);
     {match, _} ->
-      case start_with(Data, "defmodule Elena.") or start_with(Data, "defmodule Mix.") of
+      case start_with(Data, "defmodule " ++ Project_Name ++ ".") or start_with(Data, "defmodule Mix.") of
         true -> find_module(file:read_line(Content), Content, MP, [module_name(Data) | Modules]);
         false -> find_module(file:read_line(Content), Content, MP, Modules)
       end
